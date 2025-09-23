45a1b3f73ab6a1880976a669c1ef79b534301118
Kishan Thanki <kishanthankiat20@gmail.com>
2025-09-23 17:56:43 +0530

    add security

--- backend/app/routers/auth.py
+++ backend/app/routers/auth.py
@@ -1,56 +1,12 @@
 import uuid
 import aiofiles
-from jose import jwt, JWTError
-from passlib.context import CryptContext
-from datetime import datetime, timedelta
-from fastapi import APIRouter, Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from fastapi import APIRouter, Depends, HTTPException, status, File, UploadFile, Form
-
 from .. import schemas
-from ..database import users_collection 
-
-SECRET_KEY = "your-secret-key"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+from ..database import users_collection
+from fastapi.security import OAuth2PasswordRequestForm
+from fastapi import APIRouter, Depends, HTTPException, status, File, UploadFile, Form
+from ..security import verify_password, create_access_token, get_current_user, get_password_hash
 
 router = APIRouter(tags=["Authentication"])
-
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
-
-def get_password_hash(password):
-    return pwd_context.hash(password)
-
-def create_access_token(data: dict):
-    to_encode = data.copy()
-    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    to_encode.update({"exp": expire})
-    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-
-
-async def get_current_user(token: str = Depends(oauth2_scheme)):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        email: str = payload.get("sub")
-        if email is None:
-            raise credentials_exception
-    except JWTError:
-        raise credentials_exception
-    
-    user = await users_collection.find_one({"email": email})
-    if user is None:
-        raise credentials_exception
-    return user
-
 
 @router.post("/register")
 async def register(
@@ -68,10 +24,9 @@ async def register(
         content = await profile_image.read()
         await out_file.write(content)
 
-    hashed_password = get_password_hash(password)
     new_user = {
         "email": email,
-        "hashed_password": hashed_password,
+        "hashed_password": get_password_hash(password),
         "display_name": display_name,
         "profile_image_path": image_path
     }
